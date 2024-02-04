1b73f873ffb7fc1791e83d025ad7975e07b757d2
PRPetkov22 <132440546+PRPetkov22@users.noreply.github.com>
2024-02-04 18:30:07 +0200

    Add background and button to main menu

--- PDA/main/main.cpp
+++ PDA/main/main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
 #include"mainMenu.h"
+#include"raylib.h"
 using namespace std;
 
 int main()
 {
 	menu();
+	return 0;
 }
--- PDA/main/mainMenu.cpp
+++ PDA/main/mainMenu.cpp
@@ -1,13 +1,39 @@
 #include"mainMenu.h"
-#include<iostream>
-
-
-static void button() {
-
-	std::cout << "See our system" << std::endl;
-}
+#include "raylib.h"
 
 void menu() {
-	std::cout << "Hello" << std::endl;
-	button();
+	const int screenWidth = 1920;
+	const int screenHeight = 1080;
+
+	Rectangle button = { (screenWidth / 2) - 110, (screenHeight / 2) - 150, 630, 90 };
+
+	InitWindow(screenWidth, screenHeight, "Planet Dawn");
+	SetTargetFPS(60);
+
+	Texture2D resizedBackground;
+	Image bg = LoadImage("../assets/background.png");
+	ImageResize(&bg,GetScreenWidth(), GetScreenHeight());
+	resizedBackground = LoadTextureFromImage(bg);
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+
+		DrawTexture(resizedBackground, 0, 0, WHITE);
+
+
+		Vector2 mousePosition = GetMousePosition();
+		bool isMouseOverButton = CheckCollisionPointRec(mousePosition, button);
+		DrawRectangleRec(button, (isMouseOverButton ?  PINK : RED));
+		DrawText("See our solar system!", (screenWidth / 2) - 75, (screenHeight / 2) - 130 , 50, BLACK);
+		if (isMouseOverButton && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+
+		}
+
+		EndDrawing();
+
+	}
+	UnloadImage(bg);
+	UnloadTexture(resizedBackground);
+	CloseWindow();
 }
+
