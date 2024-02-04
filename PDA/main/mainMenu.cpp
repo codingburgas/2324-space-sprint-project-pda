9a08eaae1eddea3ff66544f19b5cd1bbb0d0d28c
PRPetkov22 <132440546+PRPetkov22@users.noreply.github.com>
2024-02-04 20:58:12 +0200

    Add heading to main menu and fix button

--- PDA/main/mainMenu.cpp
+++ PDA/main/mainMenu.cpp
@@ -1,11 +1,23 @@
 #include"mainMenu.h"
 #include "raylib.h"
 
+void planetsMenu() {
+
+	const int screenWidth = 1920;
+	const int screenHeight = 1080;
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		DrawRectangle(0, 0, screenWidth, screenHeight, WHITE);
+		EndDrawing();
+	}
+}
+
 void menu() {
 	const int screenWidth = 1920;
 	const int screenHeight = 1080;
 
-	Rectangle button = { (screenWidth / 2) - 110, (screenHeight / 2) - 150, 630, 90 };
+	Rectangle button = { (screenWidth / 2) - 310, (screenHeight / 2) - 55, 630, 90 };
 
 	InitWindow(screenWidth, screenHeight, "Planet Dawn");
 	SetTargetFPS(60);
@@ -20,20 +32,21 @@ void menu() {
 
 		DrawTexture(resizedBackground, 0, 0, WHITE);
 
-
 		Vector2 mousePosition = GetMousePosition();
 		bool isMouseOverButton = CheckCollisionPointRec(mousePosition, button);
-		DrawRectangleRec(button, (isMouseOverButton ?  PINK : RED));
-		DrawText("See our solar system!", (screenWidth / 2) - 75, (screenHeight / 2) - 130 , 50, BLACK);
+		DrawRectangleRec(button, (isMouseOverButton ? LIGHTGRAY : WHITE));
+		DrawText("See our solar system!", (screenWidth / 2) - 277, (screenHeight / 2) - 36, 50, BLACK);
 		if (isMouseOverButton && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-
+			planetsMenu();
 		}
+		DrawText("Planet Dawn", (screenWidth / 2) - 300, screenHeight - 800, 100, WHITE);
+		DrawRectangle((screenWidth / 2) - 310, (screenHeight / 2) - 129.5, 630,9, WHITE);
 
 		EndDrawing();
-
 	}
 	UnloadImage(bg);
 	UnloadTexture(resizedBackground);
 	CloseWindow();
 }
+
 
