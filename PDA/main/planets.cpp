e02ba12583fdf23cbc48249fcb3ec9ca550348d2
[PRPetkov22] <[PRPetkov22@codingburgas.bg]>
2024-02-12 12:22:50 +0200

    Fix the variables

--- PDA/main/planets.cpp
+++ PDA/main/planets.cpp
@@ -2,97 +2,96 @@
 
 void mercury() {
 
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         DrawRectangle(0, 0, screenWidth, screenHeight, WHITE);
-        DrawText("tova e mercury", 50, 50, 10, GREEN);
+        DrawText("tova e mercury", 50, 50, 100, GREEN);
         EndDrawing();
     }
 }
 void venus() {
 
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         DrawRectangle(0, 0, screenWidth, screenHeight, WHITE);
-        DrawText("tova e venus", 50, 50, 10, GREEN);
+        DrawText("tova e venus", 50, 50, 100, GREEN);
         EndDrawing();
     }
 }
 void earth() {
 
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         DrawRectangle(0, 0, screenWidth, screenHeight, WHITE);
-        DrawText("tova e earth", 50, 50, 10, GREEN);
+        DrawText("tova e earth", 50, 50, 100, GREEN);
         EndDrawing();
     }
 }
 void mars() {
 
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         DrawRectangle(0, 0, screenWidth, screenHeight, WHITE);
-        DrawText("tova e mars", 50, 50, 10, GREEN);
+        DrawText("tova e mars", 50, 50, 100, GREEN);
         EndDrawing();
     }
 }
 void jupiter() {
 
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         DrawRectangle(0, 0, screenWidth, screenHeight, WHITE);
-        DrawText("tova e jupiter", 50, 50, 10, GREEN);
+        DrawText("tova e jupiter", 50, 50, 100, GREEN);
         EndDrawing();
     }
 }
 void saturn() {
 
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
-
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
     while (!WindowShouldClose()) {
         BeginDrawing();
         DrawRectangle(0, 0, screenWidth, screenHeight, WHITE);
-        DrawText("tova e saturn", 50, 50, 10, GREEN);
+        DrawText("tova e saturn", 50, 50, 100, GREEN);
         EndDrawing();
     }
 }
 void uranus() {
 
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         DrawRectangle(0, 0, screenWidth, screenHeight, WHITE);
-        DrawText("tova e uranus", 50, 50, 10, GREEN);
+        DrawText("tova e uranus", 50, 50, 100, GREEN);
         EndDrawing();
     }
 }
 void neptune() {
 
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         DrawRectangle(0, 0, screenWidth, screenHeight, WHITE);
-        DrawText("tova e neptune", 50, 50, 10, GREEN);
+        DrawText("tova e neptune", 50, 50, 100, GREEN);
         EndDrawing();
     }
 }
